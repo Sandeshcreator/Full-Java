c0aeb6bb30ea53a6dee55915027dd544cb7dc4a9
Sandeshcreator <mainalisandesh8@gmail.com>
2024-03-25 19:06:27 +0545

    Learned Spring Boot annotation and  made db file

---
+++ CoreJava/src/com/SS.java
@@ -0,0 +1,9 @@
+public class SS {
+    public static void main(String[] args) {
+        int[] my_array = { 5, 4, 6, 1, 3, 2, 7, 8, 9 };
+
+
+        System.out.println(my_array[2]);
+
+    }
+}
---
+++ SpringBoot/colours/src/main/java/com/deviro/maven/services/impl/SpanishGreenPrinter.java
@@ -0,0 +1,12 @@
+package com.deviro.maven.services.impl;
+
+import com.deviro.maven.services.GreenPrinter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpanishGreenPrinter implements GreenPrinter {
+    @Override
+    public String print() {
+        return "verde ";
+    }
+}
